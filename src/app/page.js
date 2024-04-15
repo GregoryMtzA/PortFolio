e069061d4c1dea1f67e6929de702fc058be61d68
IsselCode <isselcode@gmail.com>
2024-04-15 15:13:43 -0600

    responsive corregido 2

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
       <main className="flex min-h-screen flex-col bg-[#121212] overflow-x-hidden">
         <Navbar />
-        <div className="mx-auto px-12 py-4 container">
+        <div className="mx-auto px-12 lg:py-4 py-20 container">
           <HeroSection></HeroSection>
           <ArchievementSection></ArchievementSection>
           <AboutSection></AboutSection>
